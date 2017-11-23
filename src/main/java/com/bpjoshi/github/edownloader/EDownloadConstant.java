8f5c83cbf132b5c0cd8d6f7a9802d3b7884bc694
PRASAD Bhagwati <bhagwati.prasad@soprasteria.com>
2017-11-23 18:09:18 +0530

    File for adding download properties added

--- src/main/java/com/bpjoshi/github/edownloader/EDownloadConstant.java
+++
@@ -1,8 +0,0 @@
-package com.bpjoshi.github.edownloader;
-
-public final class EDownloadConstant {
-	public final static String HTTPS_PROXY_HOST="https.proxyHost";
-	public final static String HTTPS_PROXY_PORT="https.proxyPort";
-	public final static String HTTP_PROXY_HOST="http.proxyHost";
-	public final static String HTTP_PROXY_PORT="http.proxyPort";
-}
---
+++ src/main/java/com/bpjoshi/github/edownloader/EDownloadProps.java
@@ -0,0 +1,64 @@
+package com.bpjoshi.github.edownloader;
+/**
+ * Class for setting properties for file to be downloaded.
+ * @author bpjoshi(Bhagwati Prasad)
+ *
+ */
+public class EDownloadProps {
+	private String sourceUrl;
+	private String username;
+	private String password;
+	private String fullFileName;
+	private String https_Proxy_Host;
+	private String http_Proxy_Host;
+	private String https_Proxy_Port;
+	private String http_Proxy_Port;
+	public String getSourceUrl() {
+		return sourceUrl;
+	}
+	public void setSourceUrl(String sourceUrl) {
+		this.sourceUrl = sourceUrl;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullFileName() {
+		return fullFileName;
+	}
+	public void setFullFileName(String fullFileName) {
+		this.fullFileName = fullFileName;
+	}
+	public String getHttps_Proxy_Host() {
+		return https_Proxy_Host;
+	}
+	public void setHttps_Proxy_Host(String https_Proxy_Host) {
+		this.https_Proxy_Host = https_Proxy_Host;
+	}
+	public String getHttp_Proxy_Host() {
+		return http_Proxy_Host;
+	}
+	public void setHttp_Proxy_Host(String http_Proxy_Host) {
+		this.http_Proxy_Host = http_Proxy_Host;
+	}
+	public String getHttps_Proxy_Port() {
+		return https_Proxy_Port;
+	}
+	public void setHttps_Proxy_Port(String https_Proxy_Port) {
+		this.https_Proxy_Port = https_Proxy_Port;
+	}
+	public String getHttp_Proxy_Port() {
+		return http_Proxy_Port;
+	}
+	public void setHttp_Proxy_Port(String http_Proxy_Port) {
+		this.http_Proxy_Port = http_Proxy_Port;
+	}
+}
